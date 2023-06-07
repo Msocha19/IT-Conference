489f94dccf1c123124679762bbf8cbb37609ab74
230785 Sochacki Mateusz <230785@edu.p.lodz.pl>
2023-06-07 21:01:06 +0200

    Project initialization

---
+++ src/main/java/sii/task/conference/ITConferenceApplication.java
@@ -0,0 +1,12 @@
+package sii.task.conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITConferenceApplication.class, args);
+	}
+}
